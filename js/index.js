20886d3437b39366dbcb6c0c06e07655cf4abda4
William Jordan <WilliamKatahdin@gmail.com>
2022-12-10 02:09:38 -0700

    home.html for ux class

--- js/index.js
+++ js/index.js
@@ -1,30 +1,75 @@
+let panes = document.getElementsByClassName('list-pane');
+for (let i = 0; i < panes.length; i++) {
+    panes[i].onclick = function () {
+        document.getElementsByClassName('panel')[i].classList.add("selected");
+        panes[0].classList.remove("listItem");
+        panes[1].classList.remove("listItem");
+        panes[2].classList.remove("listItem");
+        panes[3].classList.remove("listItem");
+        panes[0].classList.add("slideLeft");
+        panes[1].classList.add("slideLeft");
+        panes[2].classList.add("slideRight");
+        panes[3].classList.add("slideRight");
+        setTimeout(
+            function() {
+                panes[2].style.display='none';
+                panes[3].style.display='none';
+            }, 2000);
+
+        panes[0].style.animationDelay = "0s";
+        panes[1].style.animationDelay = "0.1s";
+        panes[2].style.animationDelay = "0.1s";
+        panes[3].style.animationDelay = "0s";
+
+        document.getElementById('click').style.display="none";
+        document.getElementById('big').style.display="flex";
+    }
+}
+
+let panels = document.getElementsByClassName('panel');
+for (let i = 0; i < panels.length; i++) {
+    panels[i].onclick = function () {
+        document.getElementsByClassName("selected")[0].classList.remove("selected");
+        this.classList.add("selected");
+    }
+}
+
+
 let selected = 1;
 let numDots = document.getElementsByClassName("dot").length;
 
 document.getElementById('d1').style.backgroundColor = "white";
 
 document.getElementById('d1').onclick = () => {
+    selected = 1;
     let tape = document.getElementById('tape');
     tape.style.translate = "-76vw";
     fillDot('d1');
+    resetTimer();
 }
 
 document.getElementById('d2').onclick = () => {
+    selected = 2;
     let tape = document.getElementById('tape');
     tape.style.translate = "-126.4vw";
     fillDot('d2');
+    resetTimer();
 }
 
 document.getElementById('d3').onclick = () => {
+    selected = 3;
     let tape = document.getElementById('tape');
     tape.style.translate = "-176.6vw";
     fillDot('d3');
+    resetTimer();
 }
 
 document.getElementById('d4').onclick = () => {
+    selected = 4;
     let tape = document.getElementById('tape');
     tape.style.translate = "-226.9vw";
     fillDot('d4');
+    resetTimer();
 }
 
 function fillDot(id) {
@@ -87,3 +132,7 @@ function resetTimer() {
         document.getElementById('right').click();
     }, 5000);
 }
+
+
+//https://coolcssanimation.com/how-to-trigger-a-css-animation-on-scroll/
+//fix do dot clicking and make it all use the same equation
